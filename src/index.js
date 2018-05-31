9507a74c5ffe62f9d47ee6f378d5dad17400e586
David Brokaw <dgbrokaw2@gmail.com>
2018-05-30 21:20:25 -0400

    installed Bootstrap

--- src/index.js
+++ src/index.js
@@ -1,6 +1,9 @@
 import David from "./my-module.js";
 import Algebrite from "algebrite";
 
+import "bootstrap";
+import "./scss/index.scss";
+
 console.log("The following are called from the entry point of this page's bundle.");
 
 // Defined within src.
@@ -12,3 +15,11 @@ console.log("Imported Algebrite adding x + x: " + Algebrite.run('x + x'));
 // JQuery and D3 imported by webpack's "ProvidePlugin": https://webpack.js.org/plugins/provide-plugin/.
 console.log("Do we have jQuery as a global? " + !!$);
 console.log("Do we have D3 as a global? " + !!d3);
+
+// Bootstrap JS imported by the import "bootstrap" statement above.
+$('.alert').alert();
+console.log("Bootstrap is letting you close the alert.");
+setTimeout(function() {
+    $(".alert").alert("close");
+    console.log("Bootstrap closed the alert for you.");
+}, 5000);
